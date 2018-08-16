7d0715e91cacc5536a7bae8d20ca6c178c1c7f0f
bmle <bmle@users.noreply.github.com>
2018-08-16 12:13:55 -0400

    More code-level documentation

--- read_fasta.py
+++ read_fasta.py
@@ -14,8 +14,10 @@ def read_fasta(filename):
     f.close()
     return sequence
 
+# print friendly message if used incorrectly
 if len(sys.argv) < 2:
     print('Usage:', sys.argv[0], '<sequence.fa>')
     exit(1)
 
+# use file name from command line
 print(read_fasta(sys.argv[1]))
